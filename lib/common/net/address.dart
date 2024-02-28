bfa0e06e96e626552dd7117a1c2359b88dad746a
大熊 <androidzk@163.com>
2024-02-28 17:20:35 +0800

    运行

--- lib/common/net/address.dart
+++ lib/common/net/address.dart
@@ -6,7 +6,8 @@ class Address {
   static const String host = "https://api.github.com/";
   static const String hostWeb = "https://github.com/";
   static const String graphicHost = 'https://ghchart.rshah.org/';
-  static const String updateUrl = 'https://gitee.com/CarGuo/GSYGithubAppFlutter/releases';
+  static const String updateUrl =
+      'https://gitee.com/CarGuo/GSYGithubAppFlutter/releases';
 
   ///获取授权  post
   static getAuthorization() {
@@ -177,8 +178,7 @@ class Address {
         reposNameFullName +
         "/" +
         "readme" +
-        ((curBranch == null || curBranch == "" ) ? "" : ("?ref=" + curBranch));
-
+        ((curBranch == null || curBranch == "") ? "" : ("?ref=" + curBranch));
   }
 
   ///我的用户信息 GET
